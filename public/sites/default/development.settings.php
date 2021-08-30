991a8e5e14ccd0955a1a616c3cf94dba877823f7
tuutti <tuutti@iki.fi>
2021-08-30 14:29:16 +0300

    UHF-2060: Added missing test and development proxy path overrides

---
+++ public/sites/default/development.settings.php
@@ -0,0 +1,8 @@
+<?php
+
+$config['helfi_proxy.settings']['prefixes'] = [
+  'en' => 'dev-social-health',
+  'fi' => 'dev-sosiaali-ja-terveyspalvelut',
+  'sv' => 'dev-social-och-halso',
+  'ru' => 'dev-social-and-health',
+];
---
+++ public/sites/default/testing.settings.php
@@ -0,0 +1,9 @@
+<?php
+
+$config['helfi_proxy.settings']['prefixes'] = [
+  'en' => 'test-social-health',
+  'fi' => 'test-sosiaali-ja-terveyspalvelut',
+  'sv' => 'test-social-och-halso',
+  'ru' => 'test-social-and-health',
+];
+
