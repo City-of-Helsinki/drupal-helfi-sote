48f74e3632b46122ae03f9d775fc9781d22a4bc4
tuutti <tuutti@iki.fi>
2024-02-13 11:11:28 +0200

    UHF-9546: Removed unnecessary settings

--- public/sites/default/testing.settings.php
+++ public/sites/default/testing.settings.php
@@ -1,11 +1,3 @@
 <?php
 
 $config['helfi_proxy.settings']['tunnistamo_return_url'] = '/fi/sosiaali-ja-terveyspalvelut/openid-connect/tunnistamo';
-$config['helfi_proxy.settings']['asset_path'] = 'terveys-assets';
-$config['helfi_proxy.settings']['prefixes'] = [
-  'en' => 'health-and-social-services',
-  'fi' => 'sosiaali-ja-terveyspalvelut',
-  'sv' => 'social-och-halsovardstjanster',
-  'ru' => 'health-and-social-services',
-  'zxx' => 'health-and-social-services',
-];
